f1299183360d3c88ad1a8033b3aec1364f966119
gurinderrrr <gurindersinghnnn@gmail.com>
2024-08-01 22:34:15 +0530

    created mah dist shp file with epsg 4326 coordinate system

--- creating mah district shapefile.py
+++ creating mah district shapefile.py
@@ -19,38 +19,41 @@ mah=map_df.loc[map_df['STATE'] == "MAH>R>SHTRA"].sort_values(['District'])
 
 mah['District'] = mah['District'].str.upper()
 
-                       #replace names
-mah.columns =mah.columns.str.replace('District', 'DISTRICT',regex=False)
+# Rename column from 'District' to 'DISTRICT'
+mah.rename(columns={'District': 'DISTRICT'}, inplace=True)
 
 
 #print(mah)
 
-mah['DISTRICT'].replace({'AHAMADNAGAR': 'AHMEDNAGAR',
-                             'AMAR>VATI': 'AMRAVATI',
-                               'AURANG>B>D': 'AURANGABAD',
-                                 'BHAND>RA': 'BHANDARA',
-                              'BULDH>NA': 'BULDHANA', 
-                               'B|D': 'BEED',
-                               'PARLI-VAI':'PARALIVAIJANATH',
-                              'J>LNA': 'JALNA',
-                               'KOLH>PUR': 'KOLHAPUR',
-                               'L>T@R': 'LATUR',
-                               'MUMBAI CITY':'MUMBAI_CITY',
-                               'SUB URBAN MUMBAI':'MUMBAI_SUBURBUN',
-                                'N>GPUR': 'NAGPUR',
-                               'N>NDED': 'NANDED',
-                                'N>SHIK': 'NASHIK',
-                               'NANDURB>R': 'NANDURBAR',
-                               'P>LGHAR': 'PALGHAR',
-                               'R>YGAD': 'RAIGAD',
-                               'RATN>GIRI': 'RATNAGIRI',
-                               'S>NGLI': 'SANGLI',
-                               'S>T>RA': 'SATARA',
-                               'SOL>PUR': 'SOLAPUR',
-                               'TH>NE': 'THANE',
-                               'USM>N>B>D': 'OSMANABAD',
-                               'W>SH|M': 'WASHIM',
-                               'YAVATM>L': 'YAVATMAL'},inplace=True)
+# Correct district names
+mah['DISTRICT'] = mah['DISTRICT'].replace({
+    'AHAMADNAGAR': 'AHMEDNAGAR',
+    'AMAR>VATI': 'AMRAVATI',
+    'AURANG>B>D': 'AURANGABAD',
+    'BHAND>RA': 'BHANDARA',
+    'BULDH>NA': 'BULDHANA', 
+    'B|D': 'BEED',
+    'PARLI-VAI': 'PARALIVAIJANATH',
+    'J>LNA': 'JALNA',
+    'KOLH>PUR': 'KOLHAPUR',
+    'L>T@R': 'LATUR',
+    'MUMBAI CITY': 'MUMBAI_CITY',
+    'SUB URBAN MUMBAI': 'MUMBAI_SUBURBUN',
+    'N>GPUR': 'NAGPUR',
+    'N>NDED': 'NANDED',
+    'N>SHIK': 'NASHIK',
+    'NANDURB>R': 'NANDURBAR',
+    'P>LGHAR': 'PALGHAR',
+    'R>YGAD': 'RAIGAD',
+    'RATN>GIRI': 'RATNAGIRI',
+    'S>NGLI': 'SANGLI',
+    'S>T>RA': 'SATARA',
+    'SOL>PUR': 'SOLAPUR',
+    'TH>NE': 'THANE',
+    'USM>N>B>D': 'OSMANABAD',
+    'W>SH|M': 'WASHIM',
+    'YAVATM>L': 'YAVATMAL'
+})
 
 #mah.drop(mah_drop, inplace=True)
 
@@ -73,10 +76,14 @@ mah = mah[['DISTRICT','geometry']]
 
 mah = mah.to_crs(epsg=4326)
 
+print(mah.crs)
+print(mah.geometry.type)
+print(mah['geometry'].head(1))
+
 
 
 #mah.to_excel('C:\\Users\\hp\\Desktop\\shape test.xlsx')
 
 
 # Step 4: Save the subset as a new shapefile
-mah.to_file('C:\\Users\\hp\\Desktop\\gurinder\\filtered shape files\\maharashtra district excluding vidarbha.shp')
+mah.to_file('C:\\Users\\hp\\Desktop\\gurinder\\python test\\maharashtra district excluding vidarbha.shp')
