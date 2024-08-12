199b80ae0b83bbe66cda0bb990fb72a88189ac24
gurinderrrr <gurindersinghnnn@gmail.com>
2024-08-12 22:57:03 +0530

    ADD AND MOD

--- ogimet test.py
+++ ogimet test.py
@@ -1,25 +1,42 @@
 import pandas as pd
 import os
+from datetime import date,datetime
+from datetime import timedelta
+import os
+import time
+
+# Get today's date
+t_day = date.today()
+
+# Separate year, month, and day as strings
+year_str = t_day.strftime("%Y")  # Year as a string
+month_str = t_day.strftime("%m")  # Month as a string
+day_str = t_day.strftime("%d")    # Day as a string
 
 # Reading the HTML table into a DataFrame
-df = pd.read_html('https://www.ogimet.com/cgi-bin/gsynres?lang=en&ord=DIR&ndays=1&ano=2024&mes=08&day=12&hora=03&ind=43057')[2]
+df = pd.read_html(f'https://www.ogimet.com/cgi-bin/gsynres?lang=en&ord=DIR&ndays=1&ano={year_str}&mes={month_str}&day={day_str}&hora=03&state=India')[2]
 
 print(df)
 print(df.info())
+
+
+
 
 # Correct column selection based on the actual structure of the DataFrame
 # Inspecting the columns first
 print(df.columns)
 
+
+
 # Selecting the specific columns
-selected_columns = df.loc[:, [('Date', 'Date'), 
+selected_columns = df.loc[:, [('Station', 'Station'), 
                               ('Temperature (C)', 'Max'), 
                               ('Temperature (C)', 'Min'), 
-                              ('Prec. (mm)', 'Prec. (mm)'),
-                              ('Pres. s.lev (Hp)', 'Pres. s.lev (Hp)')]]
+                              ('Prec. (mm)', 'Prec. (mm)')
+                              ]]
 
 # Flattening the MultiIndex columns
-selected_columns.columns = ['Date', 'Max', 'Min', 'RF', 'pressure']
+selected_columns.columns = ['Station', 'Max', 'Min', 'RF']
 
 print(selected_columns)
 print(selected_columns.info())
