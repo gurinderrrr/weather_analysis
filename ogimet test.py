51ae3bfb33a310710a15661ab0c3ada9c4b26fda
gurinderrrr <gurindersinghnnn@gmail.com>
2024-08-19 09:03:16 +0530

    mod

--- ogimet test.py
+++ ogimet test.py
@@ -16,15 +16,15 @@ day_str = t_day.strftime("%d")    # Day as a string
 # Reading the HTML table into a DataFrame
 df = pd.read_html(f'https://www.ogimet.com/cgi-bin/gsynres?lang=en&ord=DIR&ndays=1&ano={year_str}&mes={month_str}&day={day_str}&hora=03&state=India')[2]
 
-print(df)
-print(df.info())
+#print(df)
+#print(df.info())
 
 
 
 
 # Correct column selection based on the actual structure of the DataFrame
 # Inspecting the columns first
-print(df.columns)
+#print(df.columns)
 
 
 
@@ -37,6 +37,9 @@ selected_columns = df.loc[:, [('Station', 'Station'),
 
 # Flattening the MultiIndex columns
 selected_columns.columns = ['Station', 'Max', 'Min', 'RF']
+
+                        #choose columns to include in combine_tday_mh
+#selected_columns.columns =selected_columns.columns.str.replace('DATE(YYYY-MM-DD)', 'DATE',regex=False)
 
 print(selected_columns)
 print(selected_columns.info())
