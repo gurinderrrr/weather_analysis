1039360a6df8ee3486c3a8995aa9121b536fb76a
gurinderrrr <gurindersinghnnn@gmail.com>
2024-08-22 13:43:29 +0530

    mod

--- blank.py
+++ blank.py
@@ -1,87 +1,128 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-shape_path='C:\\Users\\HP\\Desktop\\guri\\district shape\\DISTRICT_BOUNDARY.shp'
-
-gdf=gpd.read_file(shape_path)
-
-#gdf=gdf.to_crs(epsg=4326)
-
-
-mah=gdf.loc[gdf['STATE'] == "MAH>R>SHTRA"].sort_values(['District'])
-
-mah['District'] = mah['District'].str.upper()
-
-                       #replace names
-mah.columns =mah.columns.str.replace('District', 'DISTRICT',regex=False)
+# import libraries
+import pandas as pd
+import numpy as np
+import pandas.io.formats.style
+import csv
+from datetime import date,datetime
+from datetime import timedelta
+import os
+import time
 
 
-#print(mah)
+t_day = date.today()
+d1 = t_day.strftime("%Y-%m-%d")
+d1_2 = t_day.strftime("%d-%m-%Y")
+y_day=t_day-timedelta(days=1)
+d0=y_day.strftime("%Y-%m-%d")
+d0_2 = y_day.strftime("%d-%m-%Y")
 
-mah['DISTRICT'].replace({'AHAMADNAGAR': 'AHMEDNAGAR',
-                             'AMAR>VATI': 'AMRAVATI',
-                               'AURANG>B>D': 'AURANGABAD',
-                                 'BHAND>RA': 'BHANDARA',
-                              'BULDH>NA': 'BULDHANA', 
-                               'B|D': 'BEED',
-                               'PARLI-VAI':'PARALIVAIJANATH',
-                              'J>LNA': 'JALNA',
-                               'KOLH>PUR': 'KOLHAPUR',
-                               'L>T@R': 'LATUR',
-                               'MUMBAI CITY':'MUMBAI_CITY',
-                               'SUB URBAN MUMBAI':'MUMBAI_SUBURBUN',
-                                'N>GPUR': 'NAGPUR',
-                               'N>NDED': 'NANDED',
-                                'N>SHIK': 'NASHIK',
-                               'NANDURB>R': 'NANDURBAR',
-                               'P>LGHAR': 'PALGHAR',
-                               'R>YGAD': 'RAIGAD',
-                               'RATN>GIRI': 'RATNAGIRI',
-                               'S>NGLI': 'SANGLI',
-                               'S>T>RA': 'SATARA',
-                               'SOL>PUR': 'SOLAPUR',
-                               'TH>NE': 'THANE',
-                               'USM>N>B>D': 'OSMANABAD',
-                               'W>SH|M': 'WASHIM',
-                               'YAVATM>L': 'YAVATMAL'},inplace=True)
 
-#mah.drop(mah_drop, inplace=True)
 
+
+
+
+
+
+                     #generate list of aws and STATIONS
+aws_mh=["MUMBAI_COLABA","MUMBAI_SANTA_CRUZ",'RATNAGIRI','MAHABALESHWAR','SATARA','SOLAPUR']
+#Merge with all_mh to include all stations
+aws_mh_df = pd.DataFrame({'STATION': aws_mh})
+
+
+
+#Collect today's data (adjust your URLs accordingly)
+today_03_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e={d1}&f={d1}&g=03&h=00')[0]
+arg_today_03_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=ARG&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e={d1}&f={d1}&g=03&h=00')[0]
+
+#Combine the dataframes
+combine_tday_03_mh = pd.concat([today_03_mh, arg_today_03_mh], ignore_index=True)
 
                             #drop vidarbha stations
-mah_drop = mah[(mah['DISTRICT'] == 'AKOLA') | (mah['DISTRICT'] == 'AMRAVATI')|
-(mah['DISTRICT'] == 'BHANDARA') | (mah['DISTRICT'] == 'BULDHANA')|
-(mah['DISTRICT'] == 'CHANDRAPUR') | (mah['DISTRICT'] == 'GADCHIROLI')|
-(mah['DISTRICT'] == 'GONDIA') | (mah['DISTRICT'] == 'NAGPUR')|
-(mah['DISTRICT'] == 'YAVATMAL') | (mah['DISTRICT'] == 'WARDHA')|
-(mah['DISTRICT'] == 'WASHIM')].index
-mah.drop(mah_drop, inplace=True)
+mah_drop_03_today = combine_tday_03_mh[(combine_tday_03_mh['DISTRICT'] == 'AKOLA') | (combine_tday_03_mh['DISTRICT'] == 'AMRAVATI')|
+(combine_tday_03_mh['DISTRICT'] == 'BHANDARA') | (combine_tday_03_mh['DISTRICT'] == 'BULDHANA')|
+(combine_tday_03_mh['DISTRICT'] == 'CHANDRAPUR') | (combine_tday_03_mh['DISTRICT'] == 'GADCHIROLI')|
+(combine_tday_03_mh['DISTRICT'] == 'GONDIA') | (combine_tday_03_mh['DISTRICT'] == 'NAGPUR')|
+(combine_tday_03_mh['DISTRICT'] == 'YAVATMAL') | (combine_tday_03_mh['DISTRICT'] == 'WARDHA')|
+(combine_tday_03_mh['DISTRICT'] == 'WASHIM')].index
+combine_tday_03_mh.drop(mah_drop_03_today, inplace=True)
+
+
+                        #choose columns to include in combine_tday_mh
+combine_tday_03_mh=combine_tday_03_mh[['STATION','RAIN FALL CUM. SINCE 0300 UTC (mm)','TEMP DAY MIN. (\'C)']]
+
+
+                       #replace names
+combine_tday_03_mh.columns =combine_tday_03_mh.columns.str.replace('RAIN FALL CUM. SINCE 0300 UTC (mm)', 'RF',regex=False)
+combine_tday_03_mh.columns =combine_tday_03_mh.columns.str.replace('TEMP DAY MIN. (\'C)', 'MIN T',regex=False)
+
+
+
+combined_all_03_stations = aws_mh_df.merge(combine_tday_03_mh, on='STATION', how='left')
+
+#print(combined_all_03_stations)
 
 
 
 
 
 
-mah = mah[['DISTRICT','geometry']]
-
-# Plot the shapefile
-fig, ax = plt.subplots(figsize=(15, 10))
-mah.plot(ax=ax, color='lightgrey', edgecolor='black', linewidth=1)
 
 
-# Add taluka names
-for x, y, dist in zip(mah.centroid.geometry.x, mah.centroid.geometry.y, mah['DISTRICT']):
-    ax.text(x, y, dist, ha='center', va='center', fontsize=4, color='black')
 
 
-# Set x and y limits to "zoom" the area
-xmin, xmax = mah.total_bounds[0], mah.total_bounds[2]  # Get min and max x values
-ymin, ymax = mah.total_bounds[1], mah.total_bounds[3]  # Get min and max y values
 
-# Adjust these values to zoom in or out
-padding = 0.1  # Add some padding to make sure the labels fit
-ax.set_xlim(xmin - padding, xmax + padding)
-ax.set_ylim(ymin - padding, ymax + padding)
 
-#gdf.plot()
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+            #collect yesterdays aws tabular data
+yesterday_mh=pd.read_html('http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e='+d0+'&f='+d0+'&g=12&h=00')
+dfy_mh=yesterday_mh[0]
+             #collect yesterdays arg tabular data
+arg_yesterday_mh=pd.read_html('http://aws.imd.gov.in:8091/AWS/dataview.php?a=ARG&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e='+d0+'&f='+d0+'&g=12&h=00')
+arg_dfy_mh=arg_yesterday_mh[0]
+
+
+                                #combine the two dataframes
+combine_yday_mh=pd.concat([dfy_mh,arg_dfy_mh], ignore_index=True)
+
+
+
+                                #drop vidarbha stations
+mah_drop_yesterday = combine_yday_mh[(combine_yday_mh['DISTRICT'] == 'AKOLA') | (combine_yday_mh['DISTRICT'] == 'AMRAVATI')|
+               (combine_yday_mh['DISTRICT'] == 'BHANDARA') | (combine_yday_mh['DISTRICT'] == 'BULDHANA')|
+               (combine_yday_mh['DISTRICT'] == 'CHANDRAPUR') | (combine_yday_mh['DISTRICT'] == 'GADCHIROLI')|
+               (combine_yday_mh['DISTRICT'] == 'GONDIA') | (combine_yday_mh['DISTRICT'] == 'NAGPUR')|
+               (combine_yday_mh['DISTRICT'] == 'YAVATMAL') | (combine_yday_mh['DISTRICT'] == 'WARDHA')|
+               (combine_yday_mh['DISTRICT'] == 'WASHIM')].index
+combine_yday_mh.drop(mah_drop_yesterday, inplace=True)
+
+
+                            #choose columns to include in combine_yday_mh
+combine_yday_mh=combine_yday_mh[['STATION','TEMP DAY MAX. (\'C)']]
+
+
+                           #replace names
+combine_yday_mh.columns =combine_yday_mh.columns.str.replace('TEMP DAY MAX. (\'C)', 'MAX T',regex=False)
+
+
+
+
+combined_all_12_stations = aws_mh_df.merge(combine_yday_mh, on='STATION', how='left')
+
+#print(combined_all_12_stations)
+
+
+combine_final=combined_all_03_stations.merge(combined_all_12_stations, on='STATION', how='left')
+
+
+combine_final.to_excel('C:\\Users\\hp\\Desktop\\combined final.xlsx', index=False)
--- ogimet test.py
+++ ogimet test.py
@@ -8,7 +8,11 @@ import time
 
 # Get today's date
 t_day = date.today()
+d1 = t_day.strftime("%Y-%m-%d")
+d1_2 = t_day.strftime("%d-%m-%Y")
 y_day=t_day-timedelta(days=1)
+d0=y_day.strftime("%Y-%m-%d")
+d0_2 = y_day.strftime("%d-%m-%Y")
 
 # Separate year, month, and day as strings
 year_str = t_day.strftime("%Y")  # Year as a string
@@ -212,15 +216,179 @@ df_full_combined = df_combined.merge(dfy_combined, on='STATIONS', how='left')
 df_full_combined=df_full_combined[['STATIONS','Observatory Rainfall (mm)','Observatory Min Temp (°C)','Observatory Max Temp (°C)','Observatory Pressure (hPa)']]
 
 
-print(df_full_combined)
-print(df_full_combined.info())
+#print(df_full_combined)
+#print(df_full_combined.info())
 
-df_full_combined.to_excel('C:\\Users\\hp\\Desktop\\test ogi final.xlsx', index=False)
-
-
+#df_full_combined.to_excel('C:\\Users\\hp\\Desktop\\test ogi final.xlsx', index=False)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                     #generate list of aws and STATIONS
+aws_mh=["MUMBAI_COLABA","MUMBAI_SANTA_CRUZ",'RATNAGIRI','MAHABALESHWAR','SATARA','SOLAPUR']
+#Merge with all_mh to include all stations
+aws_mh_df = pd.DataFrame({'STATIONS': aws_mh})
+
+
+
+#Collect today's data (adjust your URLs accordingly)
+today_03_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e={d1}&f={d1}&g=03&h=00')[0]
+arg_today_03_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=ARG&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e={d1}&f={d1}&g=03&h=00')[0]
+
+#Combine the dataframes
+combine_tday_03_mh = pd.concat([today_03_mh, arg_today_03_mh], ignore_index=True)
+
+                            #drop vidarbha stations
+mah_drop_03_today = combine_tday_03_mh[(combine_tday_03_mh['DISTRICT'] == 'AKOLA') | (combine_tday_03_mh['DISTRICT'] == 'AMRAVATI')|
+(combine_tday_03_mh['DISTRICT'] == 'BHANDARA') | (combine_tday_03_mh['DISTRICT'] == 'BULDHANA')|
+(combine_tday_03_mh['DISTRICT'] == 'CHANDRAPUR') | (combine_tday_03_mh['DISTRICT'] == 'GADCHIROLI')|
+(combine_tday_03_mh['DISTRICT'] == 'GONDIA') | (combine_tday_03_mh['DISTRICT'] == 'NAGPUR')|
+(combine_tday_03_mh['DISTRICT'] == 'YAVATMAL') | (combine_tday_03_mh['DISTRICT'] == 'WARDHA')|
+(combine_tday_03_mh['DISTRICT'] == 'WASHIM')].index
+combine_tday_03_mh.drop(mah_drop_03_today, inplace=True)
+
+
+                        #choose columns to include in combine_tday_mh
+combine_tday_03_mh=combine_tday_03_mh[['STATION','RAIN FALL CUM. SINCE 0300 UTC (mm)','TEMP DAY MIN. (\'C)','MSLP (hPa / gpm)']]
+
+
+                       #replace names
+combine_tday_03_mh.columns =combine_tday_03_mh.columns.str.replace('STATION', 'STATIONS',regex=False)
+combine_tday_03_mh.columns =combine_tday_03_mh.columns.str.replace('RAIN FALL CUM. SINCE 0300 UTC (mm)', 'RF',regex=False)
+combine_tday_03_mh.columns =combine_tday_03_mh.columns.str.replace('TEMP DAY MIN. (\'C)', 'MIN T',regex=False)
+
+
+
+combined_all_03_stations = aws_mh_df.merge(combine_tday_03_mh, on='STATIONS', how='left')
+
+#print(combined_all_03_stations)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #collect yesterdays aws tabular data
+yesterday_mh=pd.read_html('http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e='+d0+'&f='+d0+'&g=12&h=00')
+dfy_mh=yesterday_mh[0]
+             #collect yesterdays arg tabular data
+arg_yesterday_mh=pd.read_html('http://aws.imd.gov.in:8091/AWS/dataview.php?a=ARG&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e='+d0+'&f='+d0+'&g=12&h=00')
+arg_dfy_mh=arg_yesterday_mh[0]
+
+
+                                #combine the two dataframes
+combine_yday_mh=pd.concat([dfy_mh,arg_dfy_mh], ignore_index=True)
+
+
+
+                                #drop vidarbha stations
+mah_drop_yesterday = combine_yday_mh[(combine_yday_mh['DISTRICT'] == 'AKOLA') | (combine_yday_mh['DISTRICT'] == 'AMRAVATI')|
+               (combine_yday_mh['DISTRICT'] == 'BHANDARA') | (combine_yday_mh['DISTRICT'] == 'BULDHANA')|
+               (combine_yday_mh['DISTRICT'] == 'CHANDRAPUR') | (combine_yday_mh['DISTRICT'] == 'GADCHIROLI')|
+               (combine_yday_mh['DISTRICT'] == 'GONDIA') | (combine_yday_mh['DISTRICT'] == 'NAGPUR')|
+               (combine_yday_mh['DISTRICT'] == 'YAVATMAL') | (combine_yday_mh['DISTRICT'] == 'WARDHA')|
+               (combine_yday_mh['DISTRICT'] == 'WASHIM')].index
+combine_yday_mh.drop(mah_drop_yesterday, inplace=True)
+
+
+                            #choose columns to include in combine_yday_mh
+combine_yday_mh=combine_yday_mh[['STATION','TEMP DAY MAX. (\'C)']]
+
+
+                           #replace names
+combine_yday_mh.columns =combine_yday_mh.columns.str.replace('STATION', 'STATIONS',regex=False)
+combine_yday_mh.columns =combine_yday_mh.columns.str.replace('TEMP DAY MAX. (\'C)', 'MAX T',regex=False)
+
+
+
+
+combined_all_12_stations = aws_mh_df.merge(combine_yday_mh, on='STATIONS', how='left')
+
+#print(combined_all_12_stations)
+
+
+combine_final=combined_all_03_stations.merge(combined_all_12_stations, on='STATIONS', how='left')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+final_combined = df_full_combined.merge(combine_final, on='STATIONS', how='left')
+
+final_combined=final_combined[['STATIONS','Observatory Rainfall (mm)','RF','Observatory Min Temp (°C)','MIN T','Observatory Max Temp (°C)','MAX T','Observatory Pressure (hPa)','MSLP (hPa / gpm)']]
+
+
+
+
+print(final_combined)
+print(final_combined.info())
+
+final_combined.to_excel('C:\\Users\\hp\\Desktop\\comparison.xlsx', index=False)
 
 
 
