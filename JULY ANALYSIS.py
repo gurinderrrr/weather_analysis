ca0bf556d40a8b8db6bca7cd12bc50777d7b9a6e
gurinderrrr <gurindersinghnnn@gmail.com>
2024-08-25 00:02:40 +0530

    MOD

---
+++ JULY ANALYSIS.py
@@ -0,0 +1,238 @@
+print('Importing required libraries.')
+# import libraries
+import pandas as pd
+import numpy as np
+import pandas.io.formats.style
+import csv
+from datetime import date,datetime
+from datetime import timedelta
+import os
+import time
+from win32com import client
+import warnings
+from collections import Counter
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+#warnings.filterwarnings("ignore")
+import pdfkit
+from reportlab.lib.pagesizes import letter,A4
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+import re
+
+
+print('Creating required date and time')
+
+#Create today's and yesterday's date
+t_day = date.today()
+d1 = t_day.strftime("%Y-%m-%d")
+d1_2 = t_day.strftime("%d-%m-%Y")
+d1_22 = t_day.strftime("%d-%m")
+d1_year = t_day.strftime("%Y")
+d1_month = t_day.strftime("%m-%Y")
+#print(d1_22)
+
+y_day=t_day-timedelta(days=1)
+d0=y_day.strftime("%Y-%m-%d")
+d0_2 = y_day.strftime("%d-%m-%Y")
+d0_22 = y_day.strftime("%d-%m")
+#print(d0_22)
+
+
+
+
+#Set display options to show full DataFrame
+#pd.set_option('display.max_rows', 1000000)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.max_colwidth', None)
+
+
+print('Creating AWS/ARG station list for Maharashtra')
+
+                     #generate list of aws and STATIONS
+aws_mh=["MUMBAI_COLABA","MUMBAI_SANTA_CRUZ", "PALGHAR_AWS400","MATHERAN",
+'PALGHAR_KVK','IIG_MO_ALIBAG','KARJAT','MURUD',
+'DAPOLI','RATNAGIRI','DEVGAD',
+'MULDE_AMFU','AHMEDNAGAR','KOPERGAON','RAHURI',
+'DHULE','CHOPDA','JALGAON','NANDURBAR',
+'NANDURBAR_KVK','NAVAPUR','KALWAN',
+'MALEGAON','VILHOLI','NIMGIRI_JUNNAR','CAGMO_SHIVAJINAGAR',
+'CHRIST_UNIVERSITY_LAVASA','CME_DAPODI','DPS_HADAPSAR_PUNE',
+'INS_SHIVAJI_LONAVALA','KHUTBAV_DAUND','LONIKALBHOR_HAVELI',
+'NARAYANGOAN_KRISHI_KENDRA','NIASM_BARAMATI','PASHAN_AWS_LAB',
+'RAJGURUNAGAR','TALEGAON','KOLHAPUR_AMFU','MAHABALESHWAR',
+'BGRL_KARAD','SATARA','MOHOL_KVK','SOLAPUR','AURANGABAD','AURANGABAD_KVK','RATNAGIRI_AWS400','SANGOLA_MAHAVIDYALAYA',
+'AMBEJOGAI','HINGOLI','JALNA','LATUR','NANDED','OSMANABAD',
+'TULGA_KVK','PARBHANI_AMFU','RADHANAGRI_ARS','SANGLI_KVK','SHAHADA_AWS400','BEED_PTO','UDGIR_AWS400','TONDAPUR_AWS400','SAGROLI_KVK']
+
+
+# Entries to be removed
+aws_mh_to_remove = {'MURUD','NANDURBAR'}
+
+aws_mh = list(filter(lambda x: x not in aws_mh_to_remove, aws_mh))
+
+#print(len(aws_mh))
+
+
+arg_mh=['BANDRA','DAHISAR','JUHU_AIRPORT','MUMBAI AIRPORT',
+'RAM_MANDIR','TATA POWER CHEMBUR','VIDYAVIHAR',
+'VIKHROLI','BYCULLA_MUMBAI',
+'MAHALAXMI','MATUNGA_MUMBAI','SION_MUMBAI',
+'BHAYANDER','KOPARKHAIRANE','MIRA_ROAD',
+'BHIRA','POLADPUR','IIGHQ_NEWPANVEL',
+'SAVARDE(GOLWANE)','CHIPLUN',
+'POWARWADI(BHAMBHED)','AWALEGAON',
+'VAIBHAVWADI','VENGURLA','AKKALKUWA',
+'TALODA','SHIRPUR','SHIRUR',
+'CHALISGAON','JAMNER','NIPHAD','TRIMBAKESHWAR',
+'VANI','BHANDARDARA','PARNER','SHEVGAON',
+'SHRIGONDA','BALLALWADI_JUNNAR','BLINDSCHOOL_KP_PUNE',
+'CHINCHWAD_PUNE','GIRIVAN','GUDHE_BHOR',
+'KHADAKWADI_AMBEGAON','LAVALE','MAGARPATTA_PUNE',
+'MALIN_AMBEGAON','MTI_PASHAN_PUNE','NDA_PUNE','NES_LAKADI_INDAPUR',
+'PABAL_SHIRUR','RJSPMCOP_DUDULGAON',
+'SHIVAJINAGAR_PUNE','TALEGAON_DHAMDHERE',
+'VETALE_KHED','WADGAONSHERI_PUNE','WALHE_PURANDAR',
+'PANCHGANI','PHALTAN','SHIRALA','TASGAON',
+'UMADI','GARGOTI(BHUDARGAD)','PANHALA',
+'SHAHUWADI','AKKALKOT','KARMALA',
+'GANGAPUR','KANNAD','PAITHAN','BHOKARDAN',
+'GHANSANGAVI','PARTUR',
+'PARALIVAIJANATH','KALAMB',
+'CHAKUR','NILANGA',
+'PURNA','SONPETH','AUNDHA_NAGNATH',
+'KALAMNURI','BHOKAR']
+
+
+# Entries to be removed
+arg_mh_to_remove = {'BHANDARDARA','PAITHAN',
+'PARALIVAIJANATH','SHIRUR','SHIRPUR','AUNDHA_NAGNATH',
+'KALAMNURI','GARGOTI(BHUDARGAD)','SHAHUWADI','CHAKUR','NILANGA','BHOKAR','TALODA','KALAMB','TULZAPUR','PURNA','SONPETH','UMADI','PANCHGANI','PHALTAN','AKKALKOT','KARMALA'}
+
+arg_mh = list(filter(lambda x: x not in arg_mh_to_remove, arg_mh))
+
+#print(len(arg_mh))
+
+
+all_mh=['MUMBAI_COLABA','BYCULLA_MUMBAI','MAHALAXMI','MATUNGA_MUMBAI','SION_MUMBAI','MUMBAI_SANTA_CRUZ','TATA POWER CHEMBUR','BANDRA','MUMBAI AIRPORT','VIDYAVIHAR','JUHU_AIRPORT','VIKHROLI','RAM_MANDIR','DAHISAR','KOPARKHAIRANE',
+'BHAYANDER','MIRA_ROAD','IIGHQ_NEWPANVEL','KARJAT','IIG_MO_ALIBAG','MATHERAN','BHIRA',
+'MURUD','POLADPUR','INS_SHIVAJI_LONAVALA','TALEGAON','GIRIVAN','CHINCHWAD_PUNE','MTI_PASHAN_PUNE','CME_DAPODI','RJSPMCOP_DUDULGAON','LAVALE','SHIVAJINAGAR_PUNE','PASHAN_AWS_LAB','RAJGURUNAGAR','BLINDSCHOOL_KP_PUNE','NDA_PUNE','MAGARPATTA_PUNE','WADGAONSHERI_PUNE','VETALE_KHED','DPS_HADAPSAR_PUNE','LONIKALBHOR_HAVELI','PABAL_SHIRUR','BALLALWADI_JUNNAR','KHADAKWADI_AMBEGAON','NIMGIRI_JUNNAR','TALEGAON_DHAMDHERE','NARAYANGOAN_KRISHI_KENDRA','CHRIST_UNIVERSITY_LAVASA','CAGMO_SHIVAJINAGAR','KHUTBAV_DAUND','WALHE_PURANDAR','MALIN_AMBEGAON','GUDHE_BHOR','NIASM_BARAMATI','NES_LAKADI_INDAPUR','BHANDARDARA','PARNER','KOPERGAON','SHRIGONDA',
+'AHMEDNAGAR','RAHURI','SHEVGAON','PALGHAR_AWS400','PALGHAR_KVK','VILHOLI','TRIMBAKESHWAR','NIPHAD','VANI','KALWAN','MALEGAON','DAPOLI','SAVARDE(GOLWANE)',
+'POWARWADI(BHAMBHED)','CHIPLUN','RATNAGIRI','RATNAGIRI_AWS400','MAHABALESHWAR','PANCHGANI','SATARA','PHALTAN','BGRL_KARAD','MOHOL_KVK','KARMALA','SOLAPUR','SANGOLA_MAHAVIDYALAYA',
+'AKKALKOT','KOLHAPUR_AMFU','SHAHUWADI','PANHALA','RADHANAGRI_ARS','GARGOTI(BHUDARGAD)','GANGAPUR','PAITHAN','AURANGABAD_KVK','AURANGABAD','KANNAD','CHALISGAON','CHOPDA','JALGAON','JAMNER','DHULE','SHIRPUR','SHIRALA','UMADI','TASGAON','SANGLI_KVK','AKKALKUWA','NAVAPUR','TALODA','NANDURBAR','NANDURBAR_KVK','SHAHADA_AWS400','JALNA','BHOKARDAN','GHANSANGAVI','PARTUR','VAIBHAVWADI','AWALEGAON','MULDE_AMFU','DEVGAD',
+'VENGURLA','OSMANABAD','KALAMB','TULGA_KVK','AMBEJOGAI','BEED_PTO','PARALIVAIJANATH',
+'SHIRUR','CHAKUR','LATUR','NILANGA','UDGIR_AWS400','AUNDHA_NAGNATH','HINGOLI','KALAMNURI','TONDAPUR_AWS400','PARBHANI_AMFU','SONPETH','PURNA','NANDED','SAGROLI_KVK','BHOKAR']
+
+
+# Entries to be removed
+all_mh_to_remove = {'MURUD','NANDURBAR','BHANDARDARA','PAITHAN',
+'PARALIVAIJANATH','SHIRUR','SHIRPUR','AUNDHA_NAGNATH',
+'KALAMNURI','GARGOTI(BHUDARGAD)','SHAHUWADI','CHAKUR','NILANGA','BHOKAR','TALODA','KALAMB','TULZAPUR','PURNA','SONPETH','UMADI','PANCHGANI','PHALTAN','AKKALKOT','KARMALA'}
+
+all_mh = list(filter(lambda x: x not in all_mh_to_remove, all_mh))
+
+
+print(len(all_mh))
+
+print('Checking if the AWS/ARG website is working...')
+
+try:
+    #Collect today's data (adjust your URLs accordingly)
+    today_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=AWSAGRO&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e=2024-07-01&f=2024-07-31&g=03&h=00')[0]
+    arg_today_mh = pd.read_html(f'http://aws.imd.gov.in:8091/AWS/dataview.php?a=ARG&b=MAHARASHTRA&c=ALL_DISTRICT&d=ALL_STATION&e=2024-07-01&f=2024-07-31&g=03&h=00')[0]
+
+    print('Website working.')
+
+except Exception as e:
+    print(f"Error fetching data from Website: {e}")
+    time.sleep(5)
+    exit()
+
+
+print('Filtering Data...')
+
+#Combine the dataframes
+combine_tday_mh = pd.concat([today_mh, arg_today_mh], ignore_index=True)
+
+print(combine_tday_mh['STATION'].nunique())
+
+
+                            #drop vidarbha stations
+mah_drop_today = combine_tday_mh[(combine_tday_mh['DISTRICT'] == 'AKOLA') | (combine_tday_mh['DISTRICT'] == 'AMRAVATI')|
+(combine_tday_mh['DISTRICT'] == 'BHANDARA') | (combine_tday_mh['DISTRICT'] == 'BULDHANA')|
+(combine_tday_mh['DISTRICT'] == 'CHANDRAPUR') | (combine_tday_mh['DISTRICT'] == 'GADCHIROLI')|
+(combine_tday_mh['DISTRICT'] == 'GONDIA') | (combine_tday_mh['DISTRICT'] == 'NAGPUR')|
+(combine_tday_mh['DISTRICT'] == 'YAVATMAL') | (combine_tday_mh['DISTRICT'] == 'WARDHA')|
+(combine_tday_mh['DISTRICT'] == 'WASHIM')].index
+combine_tday_mh.drop(mah_drop_today, inplace=True)
+
+print(combine_tday_mh['STATION'].nunique())
+
+
+                        #choose columns to include in combine_tday_mh
+combine_tday_mh=combine_tday_mh[['STATION','DATE(YYYY-MM-DD)','RAIN FALL CUM. SINCE 0300 UTC (mm)']]
+
+
+                       #replace names
+combine_tday_mh.columns =combine_tday_mh.columns.str.replace('DATE(YYYY-MM-DD)', 'DATE',regex=False)
+combine_tday_mh.columns =combine_tday_mh.columns.str.replace('RAIN FALL CUM. SINCE 0300 UTC (mm)', 'RF',regex=False)
+
+print(combine_tday_mh['STATION'].nunique())
+
+
+
+
+#Define the start date, end date, and frequency
+start_date = '2024-07-01'
+end_date = '2024-07-31'
+frequency = '1D'
+
+#Create a datetime range
+datetime_range = pd.date_range(start=start_date, end=end_date, freq=frequency).strftime('%d-%m-%Y')
+
+# Reverse the order
+datetime_range = datetime_range[::-1]
+
+#print(datetime_range)
+
+
+
+
+
+
+
+# Create a DataFrame with the datetime range
+datetime_df = pd.DataFrame(datetime_range, columns=['DATETIME (UTC)'])
+
+#print(datetime_df)
+#print('rows in datetime_df: ',len(datetime_df))
+
+
+# Extract unique values
+stations = df['STATIONS'].unique()
+datetimes = datetime_df['DATETIME (UTC)'].unique()
+
+# Create a DataFrame with all combinations of 'station' and 'datetime'
+all_combinations = pd.DataFrame(list(itertools.product(stations, datetimes)), columns=['STATIONS', 'DATETIME (UTC)'])
+
+# Merge with the original df to include all stations and all datetimes
+complete_combined = pd.merge(all_combinations, df, on=['STATIONS', 'DATETIME (UTC)'], how='left')
+
+
+
+
+
+
+
+#Merge with all_mh to include all stations
+all_stations_df = pd.DataFrame({'STATION': all_mh})
+combined_all_stations = all_stations_df.merge(combine_tday_mh, on='STATION', how='left')
+
+print(combined_all_stations['STATION'].nunique())
+exit()
+
+
+combine_tday_mh.to_excel('C:\\Users\\hp\\Desktop\\july test.xlsx')
